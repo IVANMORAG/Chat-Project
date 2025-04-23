aa4e962e579019fd71f207a2a8233ac1bf62093b
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-23 12:06:47 -0600

    docker-compose correcciones

--- chat-client/src/utils/config.js
+++ chat-client/src/utils/config.js
@@ -1,13 +1,13 @@
 // src/utils/config.js
-// Asegúrate de que esta configuración coincida con la de tu API Gateway
-const API_URL = 'http://192.168.1.181:3000'; // Cambia esto a la IP de tu API Gateway si es necesario
+// Esta configuración apunta al Nginx que balanceará el API Gateway
+const API_URL = 'http://192.168.1.181:8000'; // Cambiado de 3000 a 8000 (puerto Nginx)
 
 export const API_ENDPOINTS = {
   register: `${API_URL}/api/auth/register`,
   login: `${API_URL}/api/auth/login`,
   me: `${API_URL}/api/auth/me`,
   rooms: `${API_URL}/api/rooms`,
-  myRooms: `${API_URL}/api/rooms/my`, // Cambiado de /my/rooms a /my
+  myRooms: `${API_URL}/api/rooms/my`,
   joinRoom: (roomId) => `${API_URL}/api/rooms/${roomId}/join`,
   leaveRoom: (roomId) => `${API_URL}/api/rooms/${roomId}/leave`
 };
